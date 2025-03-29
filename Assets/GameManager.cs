18cbe0805fe4c2d028eb2456aa8cfe1e2059cf99
Nguyentuyenhi <tuyen9bok@gmail.com>
2025-03-29 20:26:35 +0700

    Update project

---
+++ Assets/GameManager.cs
@@ -0,0 +1,132 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public List<Tube> tubes = new List<Tube>();
+    public List<Tube> tubes2 = new List<Tube>();
+
+    public GameObject prefabTube;
+    public GameObject ListTubeGO;
+    public GameObject ListTubeGO2;
+    public GameObject prefabBall;
+    public Sprite[] spriteBalls;
+    public LevelData[] levelGame;
+    public Tube currentTube;
+    public GameObject winPanel;
+    public Button nextLevelButton;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelGame[currentLevel].CreateLevel(ref tubes);
+        levelGame[currentLevel].CreateLevell(ref tubes2);
+        winPanel.SetActive(false);
+        nextLevelButton.gameObject.SetActive(false);
+        nextLevelButton.onClick.AddListener(nextlv);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void CheckWin()
+    {
+        if (IsWin())
+        {
+            Debug.Log("win");
+            ListTubeGO.SetActive(false);
+            ListTubeGO2.SetActive(false);
+            winPanel.SetActive(true);
+            nextLevelButton.gameObject.SetActive(true);
+            ListTubeGO2.SetActive(false);
+        }
+    }
+    bool IsWin()
+    {
+        for (int i = 0; i < tubes.Count; i++)
+        {
+            if (tubes[i].isTubeNull)
+            {
+                continue;
+            }
+            if (!tubes[i].isFullTube)
+            {
+                Debug.Log("chua win");
+                return false;
+            }
+            else
+            {
+                Sprite spriteCompare = tubes[i].balls[0].GetComponent<Image>().sprite;
+                for (int j = 1; j < tubes[i].balls.Count; j++)
+                {
+                    if (tubes[i].balls[j].GetComponent<Image>().sprite != spriteCompare)
+                    {
+                        Debug.Log("chua win");
+                        return false;
+                    }
+                }
+            }
+        }
+        Debug.Log("win");
+        return true;
+    }
+    public int currentLevel = 0;
+    public void nextlv()
+    {
+
+        if (currentLevel < levelGame.Length && levelGame[currentLevel])
+        {
+            foreach (var tube in tubes)
+            {
+                Destroy(tube.gameObject);
+            }
+            foreach (var tubee in tubes2)
+            {
+                Destroy(tubee.gameObject);
+            }
+            tubes.Clear();
+            tubes2.Clear();
+
+            currentLevel++;
+            if (currentLevel <= levelGame.Length)
+            {
+                levelGame[currentLevel].CreateLevel(ref tubes);
+                levelGame[currentLevel].CreateLevell(ref tubes2);
+
+                winPanel.SetActive(false);
+                nextLevelButton.gameObject.SetActive(false);
+
+                ListTubeGO.SetActive(true);
+                ListTubeGO2.SetActive(true);
+                
+                
+                
+            }
+            else
+            {
+                Debug.Log("Game đã hoàn thành.");
+            }
+        }
+    }
+}
---
+++ Assets/LevelData.cs
@@ -0,0 +1,51 @@
+﻿﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class LevelData : ScriptableObject
+{
+   public List<string> tubeData = new List<string>();
+   public void CreateLevel(ref List<Tube> tubes)
+   {
+
+		for (int i = 0; i< 4; i++)
+		{
+			Tube tube = Instantiate(GameManager.instance.prefabTube, GameManager.instance.ListTubeGO.transform).GetComponent<Tube>();
+			tubes.Add(tube);
+			int[] idBalls = Array.ConvertAll(tubeData[i].Split(','),int.Parse);
+
+			// create ball
+			for (int j = 0; j < idBalls.Length; j++)
+			{
+				if (idBalls[j] == -1) continue;
+				// tạo bóng
+				Instantiate(GameManager.instance.prefabBall, tube.listPosGO.transform.GetChild(j).position,Quaternion.identity,tube.listBallGO.transform).GetComponent<Image>().sprite = GameManager.instance.spriteBalls[idBalls[j]] ;
+
+			}
+			tube.InitTube();
+		}
+   }
+    public void CreateLevell(ref List<Tube> tubes2)
+    {
+
+        for (int i =4 ; i < tubeData.Count; i++)
+        {
+            Tube tubee = Instantiate(GameManager.instance.prefabTube, GameManager.instance.ListTubeGO2.transform).GetComponent<Tube>();
+            tubes2.Add(tubee);
+            int[] idBalls = Array.ConvertAll(tubeData[i].Split(','), int.Parse);
+
+            // create ball
+            for (int j = 0; j < idBalls.Length; j++)
+            {
+                if (idBalls[j] == -1) continue;
+                // tạo bóng
+                Instantiate(GameManager.instance.prefabBall, tubee.listPosGO.transform.GetChild(j).position, Quaternion.identity, tubee.listBallGO.transform).GetComponent<Image>().sprite = GameManager.instance.spriteBalls[idBalls[j]];
+
+            }
+            tubee.InitTube();
+        }
+    }
+}
